febda2f2cbf98d0424240fbf9773f57094afbbb6
barry <yangliucool@icloud.com>
2016-10-29 10:13:45 +0800

    update files

---
+++ delete_ubuntu_vim.sh
@@ -0,0 +1,7 @@
+sudo apt-get remove vim-tiny vim-common vim-gui-common vim-nox
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
+
